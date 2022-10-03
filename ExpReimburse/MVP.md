31c7db01fa29430c32e933765006291e801d346f
HaizhenHuang <hhuang56@ucmerced.edu>
2022-10-03 08:46:07 -0700

    login function bug

---
+++ ExpReimburse/MVP.md
@@ -0,0 +1,9 @@
+# Users should be able to login/register
+
+# Users should have either Employee or Manager role
+
+# Users should be able to submit expense reimbursement tickets
+
+# Managers should be able to process tickets
+
+# Employees should be able to view all prior ticket submissions
---
+++ ExpReimburse/Program.cs
@@ -0,0 +1,39 @@
+﻿// users login/register
+
+string isUser;
+string userName;
+string userPassword;
+
+
+Console.WriteLine("Are you an existing user? ");
+Console.WriteLine("Y/N: ");
+isUser = Console.ReadLine();
+
+
+if(isUser == "Y" || isUser == "y"){
+
+    Console.WriteLine("Please enter your user name: ");
+    userName = Console.ReadLine();
+    Console.WriteLine("Please enter your password: ");
+    userPassword = Console.ReadLine();
+
+    login(userName, userPassword);
+
+}
+else if(isUser == "N" || isUser == "n"){
+
+    string wantRegister;
+    Console.WriteLine("Do you want to register?");
+    Console.WriteLine("Y/N: ");
+    wantRegister = Console.ReadLine();
+
+}
+
+
+
+// login function
+public static void login(string userName, string userPassword){
+    Console.WriteLine("Login Success!");
+}
+
+
