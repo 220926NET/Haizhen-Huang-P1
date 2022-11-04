fe8d4d0c66bd3893a08ba67638ae4ea98c9731da
HaizhenHuang <114379077+HaizhenHuang@users.noreply.github.com>
2022-11-04 12:51:17 -0700

    Update Program.cs

--- ExpReimburse/UI/Program.cs
+++ ExpReimburse/UI/Program.cs
@@ -14,5 +14,5 @@ bool action = false;
 Ticket ticket = DatabaseTicket.ProcessPendingTicket(ticketID,action);
 Console.WriteLine(ticket.userName+"\n"+ticket.approvalStatus+"\n"+ticket.amountExpense);
 
-Ticket ticket1 = ServiceClass.ProcessTicket(ticketID, action);
-Console.WriteLine(ticket1.userName+"\n"+ticket1.approvalStatus+"\n"+ticket1.amountExpense);
+//Ticket ticket1 = ServiceClass.ProcessTicket(ticketID, action);
+//Console.WriteLine(ticket1.userName+"\n"+ticket1.approvalStatus+"\n"+ticket1.amountExpense);
